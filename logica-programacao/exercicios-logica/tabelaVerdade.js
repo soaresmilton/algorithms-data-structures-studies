632da7ab42c16ad16e3b5cace06e02c577bfc8b3
soaresmilton <eng.milton.soares@gmail.com>
2021-10-04 17:02:13 -0300

    First Commit

---
+++ logica-programacao/exercicios-logica/tabelaVerdade.js
@@ -0,0 +1,10 @@
+const proposicaoNao = A => !A ? false : true;
+
+
+const propisicaoE = (A, B) => A && B ? true : false;
+
+const proposicaoOU = (A, B) => A || B  ? true : false;
+
+console.log(proposicaoNao(true));
+console.log(propisicaoE(true, false));
+console.log(proposicaoOU(true, true));
