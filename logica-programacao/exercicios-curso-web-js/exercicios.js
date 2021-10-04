0cc2e002126dffc24b565244aa532dfe66b00483
soaresmilton <eng.milton.soares@gmail.com>
2021-10-04 18:32:10 -0300

    Exercicios do 1 ao 8

--- logica-programacao/exercicios-curso-web-js/exercicios.js
+++ logica-programacao/exercicios-curso-web-js/exercicios.js
@@ -1,4 +1,3 @@
-//Exercício 1 - Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e com ponto de exclamação "!" no final.
 const exercicio1 = nome => `Olá, ${nome}!`;
 
 const exercicio2 = idade => {
