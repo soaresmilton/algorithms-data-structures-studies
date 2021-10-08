c0320101078639e8346632d2dc3f75f21c6284a0
soaresmilton <eng.milton.soares@gmail.com>
2021-10-08 14:21:56 -0300

    Update Name Variables

--- logica-programacao/exercicios-classicos/sumOfTwo.js
+++ logica-programacao/exercicios-classicos/sumOfTwo.js
@@ -1,17 +1,17 @@
 // You have two integer arrays, a and b, and an integer target value v. Determine whether there is a pair of numbers, where one number is taken from a and the other from b, that can be added together to get a sum of v. Return true if such a pair exists, otherwise return fals
 
-const sumOfTwo = (a, b, v) => {
-  const differences = new Set()
-  for (let i = 0; i < a.length; i++) {
-    const complement = v - a[i]
+const sumOfTwo = (arr1, arr2, goal) => {
+  const differences = new Set() //=> Evita valores duplicados
+  for (let i = 0; i < arr1.length; i++) {
+    const complement = goal - arr1[i]
     differences.add(complement);
   }
 
   console.log(differences);
 
 
-  for (let j = 0; j < b.length; j++) {
-    if (differences.has(b[j])) return true
+  for (let j = 0; j < arr2.length; j++) {
+    if (differences.has(arr2[j])) return true
   }
   return false
 }
