86e05593ebd4fb0c5ace6fa7af2f0257cf6d9d03
soaresmilton <eng.milton.soares@gmail.com>
2021-10-08 17:11:18 -0300

    Sum of Two - Google Exercise

--- logica-programacao/exercicios-classicos/sumOfTwo.js
+++ logica-programacao/exercicios-classicos/sumOfTwo.js
@@ -1,4 +1,19 @@
-// You have two integer arrays, a and b, and an integer target value v. Determine whether there is a pair of numbers, where one number is taken from a and the other from b, that can be added together to get a sum of v. Return true if such a pair exists, otherwise return fals
+// You have two integer arrays, a and b, and an integer target value v. Determine whether there is a pair of numbers, where one number is taken from a and the other from b, that can be added together to get a sum of v. Return true if such a pair exists, otherwise return false
+
+// FORCE BRUTE SOLUTION
+
+function sumOfTwo(a, b, v) {
+  for (let i = 0; i < a.length; i++) {
+    let neededValue = v - a[i];
+    for (let j = 0; j < b.length; j++) {
+      if (b[j] === neededValue) return true;
+     }
+  }
+
+  return false
+}
+
+// OPTIMIZED SOLUTION
 
 const sumOfTwo = (arr1, arr2, goal) => {
   const differences = new Set() //=> Evita valores duplicados
@@ -9,11 +24,10 @@ const sumOfTwo = (arr1, arr2, goal) => {
 
   console.log(differences);
 
-
   for (let j = 0; j < arr2.length; j++) {
     if (differences.has(arr2[j])) return true
   }
   return false
 }
 
-console.log(sumOfTwo([2, 3, 4], [-15, 6, 3], 7));
+console.log(sumOfTwo([2, 3, 4, 18], [-15, 6, 3], 7));
