e8978012b3bfa948644ab53ba768fea321d9ae04
soaresmilton <eng.milton.soares@gmail.com>
2021-10-13 16:55:53 -0300

    First Commit

---
+++ logica-programacao/exercicios-classicos/leet-code/add-two-numbers.js
@@ -0,0 +1,28 @@
+function addTwoNumbers(l1, l2) {
+  let l1Reversed = '';
+  let l2Reversed = '';
+
+  l1.reverse().forEach(element => {
+    l1Reversed += element.toString();
+  })
+
+  l2.reverse().forEach(element => {
+    l2Reversed += element.toString();
+  })
+
+  let num1 = Number(l1Reversed);
+  let num2 = Number(l2Reversed);
+  let sum = num1 + num2;
+  let reversedSum = sum.toString().split('').reverse().join('');
+  let finalArr = [];
+
+  for (let i = 0; i < reversedSum.length; i++) {
+    finalArr.push(Number(reversedSum[i]));
+  }
+  return finalArr;
+
+}
+
+const l1 = [9,9,9,9,9,9,9];
+const l2 = [9,9,9,9];
+console.log(addTwoNumbers(l1, l2));
