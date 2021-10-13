faefa20da46e33d353eee8dea5a6b28e2bd9b93b
soaresmilton <eng.milton.soares@gmail.com>
2021-10-13 14:28:50 -0300

    Correct Capitalization - Daily Byte Exercise

---
+++ logica-programacao/exercicios-classicos/daily-byte-problems/correctCapitalization.js
@@ -0,0 +1,11 @@
+function correctCapitalization(str) {
+  let lowerCase = str.toLowerCase();
+  let upperCase = str.toUpperCase();
+  let isCapitalize=  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+  return str === lowerCase || str === upperCase || str === isCapitalize;
+}
+
+const str = "E se for duas";
+
+console.log(correctCapitalization(str));
