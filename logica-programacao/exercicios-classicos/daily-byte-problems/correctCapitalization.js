64c80ee9995d65c79c1a506d9b51ded1f4f37ff6
soaresmilton <eng.milton.soares@gmail.com>
2021-10-13 14:32:34 -0300

    Fixing Identation

--- logica-programacao/exercicios-classicos/daily-byte-problems/correctCapitalization.js
+++ logica-programacao/exercicios-classicos/daily-byte-problems/correctCapitalization.js
@@ -1,7 +1,7 @@
 function correctCapitalization(str) {
   let lowerCase = str.toLowerCase();
   let upperCase = str.toUpperCase();
-  let isCapitalize=  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  let isCapitalize = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
   return str === lowerCase || str === upperCase || str === isCapitalize;
 }
