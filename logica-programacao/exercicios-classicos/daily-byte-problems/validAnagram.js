06d7563a0d27b8ef1e8feb6b75e4061d7c9c0e20
soaresmilton <eng.milton.soares@gmail.com>
2021-10-18 16:07:46 -0300

    Valid Anagram - Daily Byte Exercise

--- logica-programacao/exercicios-classicos/daily-byte-problems/validAnagram.js
+++ logica-programacao/exercicios-classicos/daily-byte-problems/validAnagram.js
@@ -1,23 +1,32 @@
-function validAnagram(s, t) {
-  let map = {};
+// function validAnagram(s, t) {
+//   let map = {};
 
+//   if (s.length !== t.length) return false;
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (!map[s[i]]) map[s[i]] = 0;
+//     map[s[i]]++;
+//   }
+
+//   for (let j = 0; j < t.length; j++) {
+//     if (!map[t[j]]) return false;
+//     map[t[j]]--;
+//   }
+
+//   return true;
+// }
+
+
+function validAnagram(s, t) {
   if (s.length !== t.length) return false;
 
-  for (let i = 0; i < s.length; i++) {
-    if (!map[s[i]]) map[s[i]] = 0;
-    map[s[i]]++;
-  }
+  let organizeS = s.split('').sort().join('');
+  let organizeT = t.split('').sort().join('');
 
-  for (let j = 0; j < t.length; j++) {
-    if (!map[t[j]]) return false;
-    map[t[j]]--;
-  }
-
-  return true;
+  return organizeS === organizeT;
 }
 
-
-const s = 'abcdzg';
-const t = 'bcfagz';
+const s = 'abdc';
+const t = 'abdc';
 
 console.log(validAnagram(s, t));
