b4f75eb7d40f6943202969b567fb4b394b031e31
soaresmilton <eng.milton.soares@gmail.com>
2021-10-24 15:55:13 -0300

    Bill Division - Hacker Rank Exercise

--- exercicios/hacker-rank/billDivision.js
+++ exercicios/hacker-rank/billDivision.js
@@ -1,14 +1,13 @@
 function bonAppetit(bill, k, b) {
   let annaBills = bill.filter(value => value !== bill[k]);
   let annaCharge = (annaBills.reduce((a, b) => a + b, 0)) / 2;
-  if(b - annaCharge === 0)  console.log('Bon Appetit');
+  return b - annaCharge === 0 ? console.log('Bon Appetit') : console.log(b - annaCharge);
+}
 
-  const sumTotal = bill.reduce((a, b) => a + b)
-  console.log(sumTotal);
+function bonAppetit(bill, k, b) {
+  const sum = bill.reduce((r, v) => r + v, 0);
   const result = (sum - bill[k]) / 2;
-  console.log(result);
-
-  return console.log(b - annaCharge);
+  return b - result === 0 ? console.log('Bon Appetit') : console.log(b - result);
 }
 
 
