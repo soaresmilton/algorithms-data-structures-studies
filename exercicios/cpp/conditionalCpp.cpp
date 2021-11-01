da1ce10a8f738d475f64713f081b66b0726af54a
soaresmilton <eng.milton.soares@gmail.com>
2021-11-01 15:51:27 -0300

    Conditional Statements

--- exercicios/cpp/conditionalCpp.cpp
+++ exercicios/cpp/conditionalCpp.cpp
@@ -1,3 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
 int main()
 {
     string n_temp;
@@ -12,4 +21,26 @@ int main()
     
     
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
