f23f6c91eca0dfac56ac3684287a6320bcb4f3f5
soaresmilton <eng.milton.soares@gmail.com>
2021-11-01 16:55:29 -0300

    Mini Max Sum - Hacker Rank

--- exercicios/hacker-rank/interview-kit/minMaxSum.js
+++ exercicios/hacker-rank/interview-kit/minMaxSum.js
@@ -1,11 +1,11 @@
 function miniMaxSum(arr) {
   const sortedArray = arr.sort((a, b) => a - b);
 
-  let removeLastNumber = sortedArray.pop()
+  const removeLastNumber = sortedArray.pop()
   const maxSum = sortedArray.reduce((a, b) => a + b, 0);
   sortedArray.push(removeLastNumber);
 
-  let removeFirstNumber = sortedArray.shift();
+  const removeFirstNumber = sortedArray.shift();
   const minSum = sortedArray.reduce((a, b) => a + b, 0);
 
   return console.log(maxSum, minSum);
