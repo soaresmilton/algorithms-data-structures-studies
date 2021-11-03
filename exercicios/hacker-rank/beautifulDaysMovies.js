246c721d5eff9d33859d61b8c9947ee7d1c595a0
soaresmilton <eng.milton.soares@gmail.com>
2021-11-02 22:19:53 -0300

    Beautiful Days Movies - Hacke Rank Exercise

---
+++ exercicios/hacker-rank/beautifulDaysMovies.js
@@ -0,0 +1,21 @@
+function beautifulDays(i, j, k) {
+  let n = i;
+  const reversedNumber = () => {
+    return Number(n.toString().split('').reverse().join(''))
+  };
+  let beautyDays = 0;
+
+
+  for (n; n <= j; n++) {
+   if(Math.abs(n - reversedNumber()) % k === 0) beautyDays++;
+  }
+
+  return beautyDays;
+}
+
+
+const i = 20;
+const j = 23;
+const k = 6;
+
+console.log(beautifulDays(i, j, k));
