f4a06818b496e03c32268ae4642883ada82d416a
soaresmilton <eng.milton.soares@gmail.com>
2021-11-23 13:53:45 -0300

    add The Hurdle Race

---
+++ exercicios/hacker-rank/hurdleRace.js
@@ -0,0 +1,15 @@
+function hurdleRace(k, height) {
+  let numberOfDoses = [];
+
+  for (let i = 0; i < height.length; i++) {
+    if (k < height[i]) numberOfDoses.push(height[i] - k);
+  }
+
+  return numberOfDoses.length === 0 ? 0 : numberOfDoses.sort((a, b) => b - a)[0];
+}
+
+const k = 4;
+
+const height = [1, 6, 3, 5, 2];
+
+console.log(hurdleRace(k, height));
